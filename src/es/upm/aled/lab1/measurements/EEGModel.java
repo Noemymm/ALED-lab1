0e55d19986fc359d8244998b50ace7ce3279c7bb
noemy.martin <noemy.martin@l235.lab.dit.upm.es>
2026-09-17 12:01:08 +0200

    practica vacia

--- src/es/upm/aled/lab1/measurements/EEGModel.java
+++ src/es/upm/aled/lab1/measurements/EEGModel.java
@@ -130,6 +130,10 @@ public class EEGModel {
 	 * @throws IOException Thrown if the file can't be written.
 	 */
 	public void saveFile(String fileName) throws IOException {
+		File f = new File(fileName);
+		
+		
+		
 		// TODO
 		
 	}
